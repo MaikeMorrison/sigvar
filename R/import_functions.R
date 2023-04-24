6538d09bafef39c1b4f4ef0b9135b0646a8753db
nalcala <alcalan@iarc.fr>
2023-04-24 17:14:45 +0200

    Add some documentation and add some bioconductor requisites

--- R/import_functions.R
+++ R/import_functions.R
@@ -1,3 +1,12 @@
+#' import_SigProfiler
+#'
+#' @param folder a folder containing SigProfilerExtractor results
+#'
+#' @return A list of tibbles containing the SigProfiler activity matrices
+#' @export
+#'
+#' @examples
+#' @importFrom readr read_tsv
 import_SigProfiler <- function(folder="."){
   # find activity files within the folder
   input_files        = list.files(folder,pattern = "Activities[_refit]*.txt",recursive = T,full.names = T)
@@ -6,7 +15,7 @@ import_SigProfiler <- function(folder="."){
   # keep only suggested solutions instead of all NMF solutions
   input_files.tokeep = grep(input_files, pattern = "Suggested" ,value = F)
   # read files
-  Qlist = lapply( input_files[input_files.tokeep], read.table,h=T)
+  Qlist = lapply( input_files[input_files.tokeep], read_tsv)
   # rename list entries
   names(Qlist) = input_files.names[input_files.tokeep]
   return(Qlist)
