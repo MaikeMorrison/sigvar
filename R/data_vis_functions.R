c271c69c9801d249b411f884ea1f46291bf8c81e
MaikeMorrison <maike.morrison@gmail.com>
2023-10-22 19:46:19 -0700

    Add plot_signature_prop function from FAVA

--- R/data_vis_functions.R
+++ R/data_vis_functions.R
@@ -58,44 +58,205 @@ Q_checker <- function(Q, K, rep) {
 }
 
 
-# plot_signature_prop -----------------------------------------------------------------
-#' Plot the signature proportion for a set of samples
+#' # plot_signature_prop -----------------------------------------------------------------
+#' #' Plot the signature proportion for a set of samples
+#' #'
+#' #'
+#' #' @param Q A dataframe, matrix, or array representing mutational signature
+#' #'   relative activities.
+#' #'   Each row represents a sample.
+#' plot_signature_prop <- function(Q){
+#'   # convert to long format
+#'   Q_long <- Q %>% mutate(Sample=paste0("P",1:nrow(Q))) %>%
+#'     tidyr::pivot_longer(-Sample,names_to = "Signature",values_to = "Proportion") %>%
+#'     mutate(Signature = factor(Signature, ordered = TRUE, levels=colnames(Q)[order(colMeans(Q),decreasing = F)]) )
+#'
+#'   # order samples
+#'   Sample_order = Q_long %>% dplyr::group_by(Signature) %>% dplyr::arrange(desc(Signature),Proportion) %>% dplyr::pull(Sample) %>% unique()
+#'   Q_long = Q_long %>% dplyr::mutate(Sample = factor(Sample,levels=Sample_order))
+#'
+#'   # create color palette
+#'   sig_palette = sigvar::sbs_palette[levels(Q_long$Signature)[levels(Q_long$Signature)%in%names(sigvar::sbs_palette)]]
+#'   sig_palette = c(sig_palette,
+#'                   PNWColors::pnw_palette("Sailboat",length(levels(Q_long$Signature))-length(sig_palette) ) %>%
+#'                     `names<-`(sort(levels(Q_long$Signature)[!levels(Q_long$Signature)%in%names(sigvar::sbs_palette)]) ))
+#'   sig_palette = sig_palette[order(names(sig_palette))]
+#'
+#'   ggplot2::ggplot(Q_long,
+#'        ggplot2::aes(x = Sample, y = Proportion,
+#'            fill = Signature, color = Signature)) +
+#'   ggplot2::geom_bar(stat = "identity") +
+#'   #facet_wrap(~ Group, scales = "free_x", ncol = 4)+
+#'   scale_color_manual(values = sig_palette) + #, breaks = rev(SBS_sigs)[1:10]) +
+#'   scale_fill_manual( values = sig_palette) +
+#'     ggplot2::theme_minimal() +
+#'     ggplot2::theme(#legend.position = "bottom",
+#'     panel.grid = ggplot2::element_blank(),
+#'     strip.background = ggplot2::element_blank(), strip.text = ggplot2::element_text(size = 11),
+#'     axis.text = ggplot2::element_blank()) +
+#'     ggplot2::ylab("") + ggplot2::xlab("Sample")
+#' }
+
+# # plot_signature_prop ----------------------------------------
+#' Visualize a relative abundance matrix as a stacked bar plot.
+#'
+#' This function enables graphical visualization of a matrix of compostional data.
+#'In the output plot, each vertical bar represents a single vector;
+#' the height of each color in the bar corresponds to the abundance of each category
+#' in that vector. Because this function produces a
+#' ggplot object, its output can be modified using
+#' standard ggplot2 syntax.
+#'
+#' @param relab_matrix A matrix with \code{I=nrow(relab_matrix)} rows, each containing \code{K=ncol(relab_matrix)} non-negative entries that sum to 1.
+#' If \code{relab_matrix} contains any metadata, it must be on the left-hand side of the matrix and the number of entries
+#' that sum to 1 (\code{K}) must be specified.
+#' @param group Optional; a string specifying the name of the column that describes which group each row (sample) belongs to. Use if \code{matrices} is a single matrix containing multiple groups of samples you wish to compare.
+#' @param time Optional; a string specifying the name of the column that describes the sampling time for each row. Include if you wish to weight FAVA by the distance between samples.
+#' @param w Optional; a vector of length \code{I} with non-negative entries that sum to 1. Entry \code{w[i]} represents the weight placed on row \code{i} in the computation of the mean abundance of each category across rows. The default value is \code{w = rep(1/nrow(relab_matrix), nrow(relab_matrix))}.
+#' @param K Optional; an integer specifying the number of categories in the data. Default is \code{K=ncol(relab_matrix)}.
+#' @param arrange Optional; controls horizontal ordering of samples and vertical ordering of categories.
+#'   If \code{arrange = TRUE} or \code{arrange = "both"}, samples are ordered by the categories of greatest
+#'   abundance and categories are ordered in decreasing abundance. If \code{arrange = "vertical"}, sample
+#'   order is unchanged but categories are ordered in decreasing abundance. If \code{arrange = "horizontal"},
+#'   samples are ordered by the most abundant categories, but category order is unchanged. If \code{arrange} is missing
+#'   or \code{arrange = FALSE}, neither order is changed.
+#' @return A ggplot object containing a bar plot visualization of the relative abundance matrix.
+#' @examples
+#'
+#' # Make an example matrix of compositional data
+#' # Each row is an individual. Rows sum to 1.
+#' population_A = matrix(c(
+#'     .5, .3, .2,
+#'     .4, .2, .4,
+#'     .5, .4, .1,
+#'     .6, .1, .3,
+#'     .2, 0, .8
+#'   ),
+#'   nrow = 5,
+#'   byrow = TRUE
+#'   )
+#'
+#'   plot_signature_prop(relab_matrix = population_A,
+#'               K = 3, # How many categories per vector?
+#'               arrange = FALSE
+#'               )
+#'   plot_signature_prop(relab_matrix = population_A,
+#'               K = 3, # How many categories per vector?
+#'               arrange = "horizontal"
+#'               )
+#'   plot_signature_prop(relab_matrix = population_A,
+#'               K = 3, # How many categories per vector?
+#'               arrange = "vertical"
+#'               )
+#'    plot_signature_prop(relab_matrix = population_A,
+#'               K = 3, # How many categories per vector?
+#'               arrange = TRUE  # could also be "both"
+#'               )
 #'
 #'
-#' @param Q A dataframe, matrix, or array representing mutational signature
-#'   relative activities.
-#'   Each row represents a sample.
-plot_signature_prop <- function(Q){
-  # convert to long format
-  Q_long <- Q %>% mutate(Sample=paste0("P",1:nrow(Q))) %>%
-    tidyr::pivot_longer(-Sample,names_to = "Signature",values_to = "Proportion") %>%
-    mutate(Signature = factor(Signature, ordered = TRUE, levels=colnames(Q)[order(colMeans(Q),decreasing = F)]) )
+#' # You can modify the plot as you would any ggplot2 object
+#' plot_signature_prop(relab_matrix = population_A,
+#'               K = 3, # How many categories per vector?
+#'               arrange = TRUE
+#'               ) +
+#'   # Below are example, optional modifications to the default plot
+#'   ggplot2::ggtitle("Population A") +
+#'   ggplot2::scale_fill_brewer("Blues") +
+#'   ggplot2::scale_color_brewer("Blues") +
+#'   ggplot2::xlab("Individuals")
+#'   # Note that both scale_fill and scale_color are needed to change the color of the bars.
+#'
+#'
+#'   # Plot a dataset which has 2 populations
+#'
+#'   population_B = matrix(c(
+#'     .9, 0, .1,
+#'     .6, .4, 0,
+#'     .7, 0, .3,
+#'     .3, .4, .3,
+#'     .5, .3, .2
+#'   ),
+#'   nrow = 5,
+#'   byrow = TRUE
+#'   )
+#'
+#'
+#'   populations_AB = cbind(data.frame(c("A", "A", "A", "A", "A",
+#'                                      "B", "B", "B", "B", "B")),
+#'                          rbind(population_A, population_B))
+#'   colnames(populations_AB) = c("population", "category_1", "category_2", "category_3")
+#'
+#'
+#'  plot_signature_prop(relab_matrix = populations_AB, group = "population")
+#'  plot_signature_prop(relab_matrix = populations_AB, group = "population", arrange = "vertical")
+#'  plot_signature_prop(relab_matrix = populations_AB, group = "population", arrange = "horizontal")
+#'  plot_signature_prop(relab_matrix = populations_AB, group = "population", arrange = "both")
+#'
+#'
+#' @importFrom dplyr %>%
+#' @importFrom dplyr arrange
+#' @importFrom dplyr select
+#' @importFrom rlang .data
+#' @export
+plot_signature_prop <- function(relab_matrix, group = NULL, time = NULL, w = NULL, K = NULL, arrange = FALSE) {
 
-  # order samples
-  Sample_order = Q_long %>% dplyr::group_by(Signature) %>% dplyr::arrange(desc(Signature),Proportion) %>% dplyr::pull(Sample) %>% unique()
-  Q_long = Q_long %>% dplyr::mutate(Sample = factor(Sample,levels=Sample_order))
+  relab_checker_out = relab_checker(relab = relab_matrix, K = K, group = group, time = time)
 
-  # create color palette
-  sig_palette = sigvar::sbs_palette[levels(Q_long$Signature)[levels(Q_long$Signature)%in%names(sigvar::sbs_palette)]]
-  sig_palette = c(sig_palette,
-                  PNWColors::pnw_palette("Sailboat",length(levels(Q_long$Signature))-length(sig_palette) ) %>%
-                    `names<-`(sort(levels(Q_long$Signature)[!levels(Q_long$Signature)%in%names(sigvar::sbs_palette)]) ))
-  sig_palette = sig_palette[order(names(sig_palette))]
+  K = ncol(relab_checker_out$relab_matrix)
 
-  ggplot2::ggplot(Q_long,
-       ggplot2::aes(x = Sample, y = Proportion,
-           fill = Signature, color = Signature)) +
-  ggplot2::geom_bar(stat = "identity") +
-  #facet_wrap(~ Group, scales = "free_x", ncol = 4)+
-  scale_color_manual(values = sig_palette) + #, breaks = rev(SBS_sigs)[1:10]) +
-  scale_fill_manual( values = sig_palette) +
-    ggplot2::theme_minimal() +
-    ggplot2::theme(#legend.position = "bottom",
-    panel.grid = ggplot2::element_blank(),
-    strip.background = ggplot2::element_blank(), strip.text = ggplot2::element_text(size = 11),
-    axis.text = ggplot2::element_blank()) +
-    ggplot2::ylab("") + ggplot2::xlab("Sample")
+  # Repeat rows to account for time or weight (w) if provided,
+  # otherwise return relab_matrix unaltered
+  relab_edited = relab_sample_weighter(relab = relab_matrix, K = K, time = time, w = w, group = group)
+
+  # Re-arrange rows or columns as specified by arrange
+  # otherwise return relab_edited unaltered
+  relab_edited = arrange_categories(relab_matrix = relab_edited,
+                                    arrange = arrange,
+                                    K = K, group = group, time = time)
+
+
+  # Generate the data to plot
+  relab_plot = dplyr::mutate(relab_edited, ID = 1:nrow(relab_edited), .before = 1)
+
+
+  start =  2 + (!is.null(group)) + (!is.null(time))
+
+  relab_plot_long = tidyr::pivot_longer(relab_plot, cols = start:ncol(relab_plot))
+
+
+  relab_plot_long$ID = factor(relab_plot_long$ID , levels = unique(relab_plot$ID), ordered = TRUE)
+  relab_plot_long$name = factor(relab_plot_long$name, levels = colnames(relab_plot)[start:ncol(relab_plot)] %>% rev, ordered = TRUE)
+
+  if(!is.null(group)){
+
+    if(is.null(relab_checker_out$group)){
+      stop("The group provided is not a column name in relab_matrix. Please provide a valid group.")
+    }
+
+    ggplot2::ggplot(data = relab_plot_long, ggplot2::aes(fill = .data$name,
+                                                         color = .data$name,
+                                                         y = .data$value,
+                                                         x = .data$ID)) +
+      ggplot2::geom_bar(position = "stack", stat = "identity",
+                        width = 1) + ggplot2::theme_void() + ggplot2::ylab("") +
+      ggplot2::theme(legend.position = "none", strip.text = ggplot2::element_text(size = 12)) +
+      ggplot2::facet_wrap(~ group, scales = "free_x")
+  }else{
+
+    ggplot2::ggplot(data = relab_plot_long, ggplot2::aes(fill = .data$name,
+                                                         color = .data$name,
+                                                         y = .data$value,
+                                                         x = .data$ID)) +
+      ggplot2::geom_bar(position = "stack", stat = "identity",
+                        width = 1) + ggplot2::theme_void() + ggplot2::ylab("") +
+      ggplot2::theme(legend.position = "none", strip.text = ggplot2::element_text(size = 12))
+
+  }
 }
+
+
+
+
 
 # plot_dots -----------------------------------------------------------------
 #' Plot the mean mutational signature activities of each group of samples
