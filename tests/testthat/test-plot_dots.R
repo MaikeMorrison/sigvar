6fb649241cbaf9092016bf72fe507d32959a4bd9
Maike macbook <maike.morrison@gmail.com>
2026-07-13 17:07:36 -0600

    Run styler::style_pkg()

--- tests/testthat/test-plot_dots.R
+++ tests/testthat/test-plot_dots.R
@@ -4,80 +4,110 @@ ds_names <- ds_info$results[, "Item"]
 ds_clean <- gsub(" .*$", "", ds_names)
 data(list = ds_clean, package = "sigvar")
 
-# define variables for use in tests 
-sig_activity = ESCC_sig_activity
-sig_activity[,-c(1:3)] = round(sig_activity[,-c(1:3)]*1000)
-group = "Country"
-K=ncol(sig_activity)-3
-max_dotsize = 5
-pivot = FALSE
-median = FALSE
-normalized = FALSE
-threshold = 0
-facet = "Incidence_Level"
+# define variables for use in tests
+sig_activity <- ESCC_sig_activity
+sig_activity[, -c(1:3)] <- round(sig_activity[, -c(1:3)] * 1000)
+group <- "Country"
+K <- ncol(sig_activity) - 3
+max_dotsize <- 5
+pivot <- FALSE
+median <- FALSE
+normalized <- FALSE
+threshold <- 0
+facet <- "Incidence_Level"
 
 
 # WITH FACET
 test_that("mean normalized works with facet", {
-  expect_warning(plot_dots(sig_activity = sig_activity, group = "Country",
-                           facet = "Incidence_Level", K = 43))
-  expect_no_warning(plot_dots(sig_activity = ESCC_sig_activity, group = "Country",
-                              facet = "Incidence_Level", K = 43))
+  expect_warning(plot_dots(
+    sig_activity = sig_activity, group = "Country",
+    facet = "Incidence_Level", K = 43
+  ))
+  expect_no_warning(plot_dots(
+    sig_activity = ESCC_sig_activity, group = "Country",
+    facet = "Incidence_Level", K = 43
+  ))
 })
 
 
 test_that("mean unnormalized works with facet", {
-  expect_no_error(plot_dots(sig_activity = sig_activity, group = "Country",
-                            facet = "Incidence_Level", normalized = FALSE, K = 43))
-  expect_no_warning(plot_dots(sig_activity = sig_activity, group = "Country",
-                              facet = "Incidence_Level", normalized = FALSE, K = 43))
+  expect_no_error(plot_dots(
+    sig_activity = sig_activity, group = "Country",
+    facet = "Incidence_Level", normalized = FALSE, K = 43
+  ))
+  expect_no_warning(plot_dots(
+    sig_activity = sig_activity, group = "Country",
+    facet = "Incidence_Level", normalized = FALSE, K = 43
+  ))
 })
 
 test_that("median normalized works with facet", {
-  expect_warning(plot_dots(sig_activity = sig_activity, median = TRUE, group = "Country",
-                           facet = "Incidence_Level", K = 43))
-  expect_no_warning(plot_dots(sig_activity = ESCC_sig_activity, median = TRUE, group = "Country",
-                              facet = "Incidence_Level", K = 43))
+  expect_warning(plot_dots(
+    sig_activity = sig_activity, median = TRUE, group = "Country",
+    facet = "Incidence_Level", K = 43
+  ))
+  expect_no_warning(plot_dots(
+    sig_activity = ESCC_sig_activity, median = TRUE, group = "Country",
+    facet = "Incidence_Level", K = 43
+  ))
 })
 
 
 test_that("median unnormalized works with facet", {
-  expect_no_error(plot_dots(sig_activity = sig_activity, median = TRUE, group = "Country",
-                            facet = "Incidence_Level", normalized = FALSE, K = 43))
-  expect_no_warning(plot_dots(sig_activity = sig_activity, median = TRUE, group = "Country",
-                              facet = "Incidence_Level", normalized = FALSE, K = 43))
+  expect_no_error(plot_dots(
+    sig_activity = sig_activity, median = TRUE, group = "Country",
+    facet = "Incidence_Level", normalized = FALSE, K = 43
+  ))
+  expect_no_warning(plot_dots(
+    sig_activity = sig_activity, median = TRUE, group = "Country",
+    facet = "Incidence_Level", normalized = FALSE, K = 43
+  ))
 })
-
 
 
 # NO FACET
 test_that("mean normalized works without facet", {
-  expect_warning(plot_dots(sig_activity = sig_activity, group = "Country",
-                           K = 43))
-  expect_no_warning(plot_dots(sig_activity = ESCC_sig_activity, group = "Country",
-                              K = 43))
+  expect_warning(plot_dots(
+    sig_activity = sig_activity, group = "Country",
+    K = 43
+  ))
+  expect_no_warning(plot_dots(
+    sig_activity = ESCC_sig_activity, group = "Country",
+    K = 43
+  ))
 })
 
 
 test_that("mean unnormalized works without facet", {
-  expect_no_error(plot_dots(sig_activity = sig_activity, group = "Country",
-                            normalized = FALSE, K = 43))
-  expect_no_warning(plot_dots(sig_activity = sig_activity, group = "Country",
-                              normalized = FALSE, K = 43))
+  expect_no_error(plot_dots(
+    sig_activity = sig_activity, group = "Country",
+    normalized = FALSE, K = 43
+  ))
+  expect_no_warning(plot_dots(
+    sig_activity = sig_activity, group = "Country",
+    normalized = FALSE, K = 43
+  ))
 })
 
 test_that("median normalized works without facet", {
-  expect_warning(plot_dots(sig_activity = sig_activity, median = TRUE, group = "Country",
-                           K = 43))
-  expect_no_warning(plot_dots(sig_activity = ESCC_sig_activity, median = TRUE, group = "Country",
-                              K = 43))
+  expect_warning(plot_dots(
+    sig_activity = sig_activity, median = TRUE, group = "Country",
+    K = 43
+  ))
+  expect_no_warning(plot_dots(
+    sig_activity = ESCC_sig_activity, median = TRUE, group = "Country",
+    K = 43
+  ))
 })
 
 
 test_that("median unnormalized works without facet", {
-  expect_no_error(plot_dots(sig_activity = sig_activity, median = TRUE, group = "Country",
-                            normalized = FALSE, K = 43))
-  expect_no_warning(plot_dots(sig_activity = sig_activity, median = TRUE, group = "Country",
-                              normalized = FALSE, K = 43))
+  expect_no_error(plot_dots(
+    sig_activity = sig_activity, median = TRUE, group = "Country",
+    normalized = FALSE, K = 43
+  ))
+  expect_no_warning(plot_dots(
+    sig_activity = sig_activity, median = TRUE, group = "Country",
+    normalized = FALSE, K = 43
+  ))
 })
-
